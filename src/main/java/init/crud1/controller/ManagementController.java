1cb69225a923b0cc2332307dcecd41871885fd5a
WeberLaurent <weberlaurent23@gmail.com>
2020-04-21 21:10:41 +0200

    Add Coverage Test Configuration

--- src/main/java/init/crud1/controller/ManagementController.java
+++ src/main/java/init/crud1/controller/ManagementController.java
@@ -1,155 +1,164 @@
 package init.crud1.controller;
 
-import init.crud1.entity.*;
-import init.crud1.form.*;
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import init.crud1.entity.Activity;
+import init.crud1.entity.NewsType;
+import init.crud1.entity.SportsMan;
+import init.crud1.form.ActivityTypeForm;
+import init.crud1.form.LevelForm;
+import init.crud1.form.SearchNewForm;
+import init.crud1.form.TopicForm;
 import init.crud1.service.ActivityService;
 import init.crud1.service.ManagementService;
 import init.crud1.service.NewsService;
 import init.crud1.service.SportsManService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @Controller
 public class ManagementController {
 
-    private ActivityService activityService;
-    private SportsManService sportsManService;
-    private ManagementService managementService;
-    private NewsService newsService;
+	private ActivityService activityService;
+	private SportsManService sportsManService;
+	private ManagementService managementService;
+	private NewsService newsService;
 
-    @Autowired
-    public ManagementController(ActivityService activityService, SportsManService sportsManService,
-                                ManagementService managementService, NewsService newsService) {
-        this.activityService = activityService;
-        this.sportsManService = sportsManService;
-        this.managementService = managementService;
-        this.newsService = newsService;
-    }
+	@Autowired
+	public ManagementController(ActivityService activityService, SportsManService sportsManService,
+			ManagementService managementService, NewsService newsService) {
+		this.activityService = activityService;
+		this.sportsManService = sportsManService;
+		this.managementService = managementService;
+		this.newsService = newsService;
+	}
 
-    @RequestMapping("/manageUsers")
-    public String manageUserSetting(Model model) {
-        TopicForm topicForm = new TopicForm();
-        model.addAttribute("allUsers", sportsManService.getAllUser());
-        model.addAttribute("allCandidates", managementService.getPromotionCandidates());
-        model.addAttribute("topicForm", topicForm);
-        return "manageUsers";
-    }
+	@RequestMapping(value="/manageUsers", method = RequestMethod.GET)
+	public String manageUserSetting(Model model) {
+		TopicForm topicForm = new TopicForm();
+		model.addAttribute("allUsers", sportsManService.getAllUser());
+		model.addAttribute("allCandidates", managementService.getPromotionCandidates());
+		model.addAttribute("topicForm", topicForm);
+		return "manageUsers";
+	}
 
-    @RequestMapping("/manageEvents")
-    public String manageEventSetting(Model model) {
-        model.addAttribute("allActivities", activityService.getAllActivities());
-        return "manageEvents";
-    }
+	@RequestMapping(value="/manageEvents", method = RequestMethod.GET)
+	public String manageEventSetting(Model model) {
+		model.addAttribute("allActivities", activityService.getAllActivities());
+		return "manageEvents";
+	}
 
-    @RequestMapping(value = "/cancel{id}", method = RequestMethod.GET)
-    public String cancel(@RequestParam(value = "id") Long id) {
-        activityService.cancelOrActivateActivity(activityService.getSpecificActivity(id), false);
-        newsService.returnCancelledApplictionNewOrCloseEventNew(activityService.getSpecificActivity(id),
-                NewsType.CANCELLED_EVENT);
-        return "redirect:/manageEvents";
-    }
+	@RequestMapping(value = "/cancel{id}", method = RequestMethod.GET)
+	public String cancel(@RequestParam(value = "id") Long id) {
+		activityService.cancelOrActivateActivity(activityService.getSpecificActivity(id), false);
+		newsService.returnCancelledApplictionNewOrCloseEventNew(activityService.getSpecificActivity(id),
+				NewsType.CANCELLED_EVENT);
+		return "redirect:/manageEvents";
+	}
 
-    @RequestMapping(value = "/open{id}", method = RequestMethod.GET)
-    public String open(@RequestParam(value = "id") Long id) {
-        activityService.cancelOrActivateActivity(activityService.getSpecificActivity(id), true);
-        return "redirect:/manageEvents";
-    }
+	@RequestMapping(value = "/open{id}", method = RequestMethod.GET)
+	public String open(@RequestParam(value = "id") Long id) {
+		activityService.cancelOrActivateActivity(activityService.getSpecificActivity(id), true);
+		return "redirect:/manageEvents";
+	}
 
-    @RequestMapping(value = "/user/block{id}", method = RequestMethod.GET)
-    public String block(@RequestParam(value = "id") Long id) {
-        sportsManService.blockOrUnblock(sportsManService.findSpecificUser(id),true);
-        for (Activity activity : sportsManService.findSpecificUser(id).getCreatedActivities()) {
-            activityService.cancelOrActivateActivity(activity,false);
-        }
-        return "redirect:/manageUsers";
-    }
+	@RequestMapping(value = "/user/block{id}", method = RequestMethod.GET)
+	public String block(@RequestParam(value = "id") Long id) {
+		sportsManService.blockOrUnblock(sportsManService.findSpecificUser(id),true);
+		for (Activity activity : sportsManService.findSpecificUser(id).getCreatedActivities()) {
+			activityService.cancelOrActivateActivity(activity,false);
+		}
+		return "redirect:/manageUsers";
+	}
 
-    @RequestMapping(value = "/user/unblock{id}", method = RequestMethod.GET)
-    public String unblock(@RequestParam(value = "id") Long id) {
-        sportsManService.blockOrUnblock(sportsManService.findSpecificUser(id),false);
-        for (Activity activity : sportsManService.findSpecificUser(id).getCreatedActivities()) {
-            activityService.cancelOrActivateActivity(activity,true);
-        }
-        return "redirect:/manageUsers";
-    }
+	@RequestMapping(value = "/user/unblock{id}", method = RequestMethod.GET)
+	public String unblock(@RequestParam(value = "id") Long id) {
+		sportsManService.blockOrUnblock(sportsManService.findSpecificUser(id),false);
+		for (Activity activity : sportsManService.findSpecificUser(id).getCreatedActivities()) {
+			activityService.cancelOrActivateActivity(activity,true);
+		}
+		return "redirect:/manageUsers";
+	}
 
-    @RequestMapping(value = "/refusePromotion{id}", method = RequestMethod.GET)
-    public String refusePromotionUser(@RequestParam(value = "id") Long id) {
-        SportsMan sportsMan = sportsManService.findSpecificUser(id);
-        managementService.removeRequest(managementService.findSpecific(sportsMan));
-        newsService.makeNews(NewsType.NEGATIVE_REQUEST, sportsMan, null);
-        return "redirect:/manageUsers";
-    }
+	@RequestMapping(value = "/refusePromotion{id}", method = RequestMethod.GET)
+	public String refusePromotionUser(@RequestParam(value = "id") Long id) {
+		SportsMan sportsMan = sportsManService.findSpecificUser(id);
+		managementService.removeRequest(managementService.findSpecific(sportsMan));
+		newsService.makeNews(NewsType.NEGATIVE_REQUEST, sportsMan, null);
+		return "redirect:/manageUsers";
+	}
 
-    @RequestMapping(value = "/promote{id}", method = RequestMethod.GET)
-    public String promoteUser(@RequestParam(value = "id") Long id) {
-        sportsManService.promoteUser(sportsManService.findSpecificUser(id));
-        managementService.removeRequest(managementService.findSpecific
-                (sportsManService.findSpecificUser(id)));
-        newsService.makeNews(NewsType.VALIDATED_REQUEST,
-                sportsManService.findSpecificUser(id), null);
-        //Add notification!!
-        return "redirect:/manageUsers";
-    }
+	@RequestMapping(value = "/promote{id}", method = RequestMethod.GET)
+	public String promoteUser(@RequestParam(value = "id") Long id) {
+		sportsManService.promoteUser(sportsManService.findSpecificUser(id));
+		managementService.removeRequest(managementService.findSpecific
+				(sportsManService.findSpecificUser(id)));
+		newsService.makeNews(NewsType.VALIDATED_REQUEST,
+				sportsManService.findSpecificUser(id), null);
+		//Add notification!!
+		return "redirect:/manageUsers";
+	}
 
-    @RequestMapping(value = "/addTopic", method = RequestMethod.POST)
-    public String createTopic(@ModelAttribute("topicForm") TopicForm topicForm,
-                              Principal principal) {
-        this.managementService.addTopic(sportsManService.findCurrentUser(principal.getName()),
-                topicForm);
-        return "redirect:/manage";
-    }
+	@RequestMapping(value = "/addTopic", method = RequestMethod.POST)
+	public String createTopic(@ModelAttribute("topicForm") TopicForm topicForm,
+			Principal principal) {
+		this.managementService.addTopic(sportsManService.findCurrentUser(principal.getName()),
+				topicForm);
+		return "redirect:/manage";
+	}
 
-    @RequestMapping(value = "/manageSportsSetting", method = RequestMethod.GET)
-    public String manageSportsSetting(Model model) {
-        model.addAttribute("activityTypeForm",new ActivityTypeForm());
-        model.addAttribute("activityTypes",activityService.getAllActivityTypes());
-        return "setSportsSetting";
-    }
+	@RequestMapping(value = "/manageSportsSetting", method = RequestMethod.GET)
+	public String manageSportsSetting(Model model) {
+		model.addAttribute("activityTypeForm",new ActivityTypeForm());
+		model.addAttribute("activityTypes",activityService.getAllActivityTypes());
+		return "setSportsSetting";
+	}
 
-    @RequestMapping(value = "/updateType{id}", method = RequestMethod.POST)
-    public String updateType(@RequestParam(value = "id") Long id, @ModelAttribute("activityTypeForm") ActivityTypeForm activityTypeForm) {
-        managementService.updateType(managementService.findSpecificActivityType(id),activityTypeForm);
-        return "redirect:/manageSportsSetting";
-    }
+	@RequestMapping(value = "/updateType{id}", method = RequestMethod.POST)
+	public String updateType(@RequestParam(value = "id") Long id, @ModelAttribute("activityTypeForm") ActivityTypeForm activityTypeForm) {
+		managementService.updateType(managementService.findSpecificActivityType(id),activityTypeForm);
+		return "redirect:/manageSportsSetting";
+	}
 
-    @RequestMapping(value = "/addType", method = RequestMethod.POST)
-    public String addType(@ModelAttribute("activityTypeForm") ActivityTypeForm activityTypeForm) {
-        managementService.createType(activityTypeForm);
-        return "redirect:/manageSportsSetting";
-    }
+	@RequestMapping(value = "/addType", method = RequestMethod.POST)
+	public String addType(@ModelAttribute("activityTypeForm") ActivityTypeForm activityTypeForm) {
+		managementService.createType(activityTypeForm);
+		return "redirect:/manageSportsSetting";
+	}
 
-    @RequestMapping(value = "/manageLevelsSetting", method = RequestMethod.GET)
-    public String manageLevelsSetting(Model model) {
-        model.addAttribute("levelForm",new LevelForm());
-        model.addAttribute("activityLevels",managementService.getAllLevels());
-        return "setLevelsSetting";
-    }
+	@RequestMapping(value = "/manageLevelsSetting", method = RequestMethod.GET)
+	public String manageLevelsSetting(Model model) {
+		model.addAttribute("levelForm",new LevelForm());
+		model.addAttribute("activityLevels",managementService.getAllLevels());
+		return "setLevelsSetting";
+	}
 
-    @RequestMapping(value = "/updateLevel{id}", method = RequestMethod.POST)
-    public String updateLevel(@RequestParam(value = "id") Long id, @ModelAttribute("levelForm") LevelForm levelForm) {
-        managementService.updateLevel(levelForm,managementService.findSpecificLevel(id));
-        return "redirect:/manageLevelsSetting";
-    }
+	@RequestMapping(value = "/updateLevel{id}", method = RequestMethod.POST)
+	public String updateLevel(@RequestParam(value = "id") Long id, @ModelAttribute("levelForm") LevelForm levelForm) {
+		managementService.updateLevel(levelForm,managementService.findSpecificLevel(id));
+		return "redirect:/manageLevelsSetting";
+	}
 
-    @RequestMapping(value = "/history")
-    public String getHistory(@ModelAttribute("searchNewForm") SearchNewForm searchNewForm,
-                             Model model, @RequestParam(required = false) Boolean there) {
-        model.addAttribute("allTypes", newsService.getAllNewsType());
-        if(there != null){ ;
-            model.addAttribute("allActs",newsService.findForSearch(searchNewForm));
-            model.addAttribute("searchActivityForm",searchNewForm);
-            return "searchNew";
-        }
-        else{
-            model.addAttribute("allActs",newsService.findAll());
-            model.addAttribute("searchActivityForm",searchNewForm);
-            return "searchNew";
-        }
-    }
+	@RequestMapping(value = "/history", method = RequestMethod.GET)
+	public String getHistory(@ModelAttribute("searchNewForm") SearchNewForm searchNewForm,
+			Model model, @RequestParam(required = false) Boolean there) {
+		model.addAttribute("allTypes", newsService.getAllNewsType());
+		if(there != null){ ;
+		model.addAttribute("allActs",newsService.findForSearch(searchNewForm));
+		model.addAttribute("searchActivityForm",searchNewForm);
+		return "searchNew";
+		}
+		else{
+			model.addAttribute("allActs",newsService.findAll());
+			model.addAttribute("searchActivityForm",searchNewForm);
+			return "searchNew";
+		}
+	}
 
 }
--- src/main/java/init/crud1/controller/SportsManController.java
+++ src/main/java/init/crud1/controller/SportsManController.java
@@ -1,142 +1,138 @@
 package init.crud1.controller;
 
-import init.crud1.entity.*;
-import init.crud1.exception.UserAlreadyExistException;
-import init.crud1.form.SportsManForm;
-import init.crud1.repository.*;
-import init.crud1.service.ActivityService;
-import init.crud1.service.ManagementService;
-import init.crud1.service.SportsManService;
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.naming.Binding;
-import javax.validation.Valid;
-import java.security.Principal;
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
+import init.crud1.exception.UserAlreadyExistException;
+import init.crud1.form.SportsManForm;
+import init.crud1.service.ActivityService;
+import init.crud1.service.ManagementService;
+import init.crud1.service.SportsManService;
 
 @Controller
 public class SportsManController {
 
-    @Autowired
-    SportsManService sportsManService;
+	@Autowired
+	SportsManService sportsManService;
 
-    @Autowired
-    ActivityService activityService;
+	@Autowired
+	ActivityService activityService;
 
-    @Autowired
-    ManagementService managementService;
+	@Autowired
+	ManagementService managementService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @RequestMapping("/users")
-    public String getSportsMen(Model model) {
-        model.addAttribute("allUsers", sportsManService.getAllUser());
-        return "users";
-    }
+	@RequestMapping(value= "/users", method = RequestMethod.GET)
+	public String getSportsMen(Model model) {
+		model.addAttribute("allUsers", sportsManService.getAllUser());
+		return "users";
+	}
 
-    @RequestMapping("/signUp")
-    public String createSportsMan(Model model) {
-        model.addAttribute("sportsManForm", new SportsManForm());
-        return "signUp";
-    }
+	@RequestMapping(value="/signUp", method = RequestMethod.GET)
+	public String createSportsMan(Model model) {
+		model.addAttribute("sportsManForm", new SportsManForm());
+		return "signUp";
+	}
 
-    @RequestMapping(value = "saveUser", method = RequestMethod.POST)
-    public String saveSportsMan(@Valid SportsManForm sportsManForm, BindingResult bindingResult) throws UserAlreadyExistException {
+	@RequestMapping(value = "saveUser", method = RequestMethod.POST)
+	public String saveSportsMan(@Valid SportsManForm sportsManForm, BindingResult bindingResult) throws UserAlreadyExistException {
 
-        if(bindingResult.hasErrors()){
-            System.out.println("Errors: " + bindingResult.getErrorCount());
-            return "signUp";
-        }
+		if(bindingResult.hasErrors()){
+			System.out.println("Errors: " + bindingResult.getErrorCount());
+			return "signUp";
+		}
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate dateInput = LocalDate.parse(sportsManForm.getDateofBirth(),formatter).plusDays(1);
-        LocalDate current = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate dateInput = LocalDate.parse(sportsManForm.getDateofBirth(),formatter).plusDays(1);
+		LocalDate current = LocalDate.now();
 
-        if(this.sportsManService.findCurrentUser(sportsManForm.getMail()) != null) {
-            bindingResult.rejectValue("mail", "", "This account already exists");
-            return "signUp";
-        }
-        else if(Period.between(dateInput,current).getYears() < 18){
-            System.out.println(Period.between(dateInput,current).getYears());
-            bindingResult.rejectValue("dateofBirth","","You must have 18 years old to register");
-            return "signUp";
-        }
-        else if(!(sportsManForm.getPassword()).equals(sportsManForm.getConfirmPassword())){
-            bindingResult.rejectValue("password","","The two passwords do not match.");
-            return "signUp";
-        }
-        else{
-            sportsManService.createUser(sportsManForm); //NB : if update de l'email, déconnexion à mettre en place
-        }                                               // ou refresh!!! (à creuser!!)
-        return "users";
-    }
+		if(this.sportsManService.findCurrentUser(sportsManForm.getMail()) != null) {
+			bindingResult.rejectValue("mail", "", "This account already exists");
+			return "signUp";
+		}
+		else if(Period.between(dateInput,current).getYears() < 18){
+			System.out.println(Period.between(dateInput,current).getYears());
+			bindingResult.rejectValue("dateofBirth","","You must have 18 years old to register");
+			return "signUp";
+		}
+		else if(!(sportsManForm.getPassword()).equals(sportsManForm.getConfirmPassword())){
+			bindingResult.rejectValue("password","","The two passwords do not match.");
+			return "signUp";
+		}
+		else{
+			sportsManService.createUser(sportsManForm); //NB : if update de l'email, déconnexion à mettre en place
+		}                                               // ou refresh!!! (à creuser!!)
+		return "users";
+	}
 
-    @RequestMapping(value = "/user/update", method = RequestMethod.GET)
-    public String updateSportsManForm(Model model, Principal principal) {
-        model.addAttribute("sportsManForm",
-                new SportsManForm(sportsManService.findCurrentUser(principal.getName())));
-        return "updateUser";
-    }
+	@RequestMapping(value = "/user/update", method = RequestMethod.GET)
+	public String updateSportsManForm(Model model, Principal principal) {
+		model.addAttribute("sportsManForm",
+				new SportsManForm(sportsManService.findCurrentUser(principal.getName())));
+		return "updateUser";
+	}
 
-    @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
-    public String updateSportsMan(@Valid SportsManForm sportsManForm,
-                                  BindingResult bindingResult, Principal principal) {
-        if(bindingResult.hasErrors()){
-            return "user";
-        }
-        sportsManService.updateUser(sportsManService.findCurrentUser(principal.getName()), sportsManForm);
-        return "users";
-    }
+	@RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+	public String updateSportsMan(@Valid SportsManForm sportsManForm,
+			BindingResult bindingResult, Principal principal) {
+		if(bindingResult.hasErrors()){
+			return "user";
+		}
+		sportsManService.updateUser(sportsManService.findCurrentUser(principal.getName()), sportsManForm);
+		return "users";
+	}
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String sportsManOwnDetails(Principal principal, Model model) {
-        model.addAttribute("sportsMan", sportsManService.findCurrentUser(principal.getName()));
-        model.addAttribute("allUsers",
-                sportsManService.getPotentialContacts(sportsManService.findCurrentUser(principal.getName())));
-        model.addAttribute("statistics",
-                sportsManService.findBySportsMan(sportsManService.findCurrentUser(principal.getName())));
-        return "userDetails";
-    }
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String sportsManOwnDetails(Principal principal, Model model) {
+		model.addAttribute("sportsMan", sportsManService.findCurrentUser(principal.getName()));
+		model.addAttribute("allUsers",
+				sportsManService.getPotentialContacts(sportsManService.findCurrentUser(principal.getName())));
+		model.addAttribute("statistics",
+				sportsManService.findBySportsMan(sportsManService.findCurrentUser(principal.getName())));
+		return "userDetails";
+	}
 
-    @RequestMapping(value = "/getRegisteredEvents{id}", method = RequestMethod.GET)
-    public String getRegisteredEvents(@RequestParam Long id, Model model) {
-        model.addAttribute("sportsMan", sportsManService.findSpecificUser(id));
-        return "userParticipatedEvents";
-    }
+	@RequestMapping(value = "/getRegisteredEvents{id}", method = RequestMethod.GET)
+	public String getRegisteredEvents(@RequestParam Long id, Model model) {
+		model.addAttribute("sportsMan", sportsManService.findSpecificUser(id));
+		return "userParticipatedEvents";
+	}
 
-    @RequestMapping(value = "/addContact{id}", method = RequestMethod.POST)
-    public String addContact(@RequestParam(value = "contact") Long idContact,
-                             Principal principal) {
-        sportsManService.addOrRemoveContacts(sportsManService.findCurrentUser(principal.getName()),
-                sportsManService.findSpecificUser(idContact), true);
-        return "redirect:/user";
-    }
+	@RequestMapping(value = "/addContact{id}", method = RequestMethod.POST)
+	public String addContact(@RequestParam(value = "contact") Long idContact,
+			Principal principal) {
+		sportsManService.addOrRemoveContacts(sportsManService.findCurrentUser(principal.getName()),
+				sportsManService.findSpecificUser(idContact), true);
+		return "redirect:/user";
+	}
 
-    @RequestMapping(value = "/removeContact{id}", method = RequestMethod.POST)
-    public String removeContact(@RequestParam(value = "contact") Long idContact,
-                                Principal principal) {
-        sportsManService.addOrRemoveContacts(sportsManService.findCurrentUser(principal.getName()),
-                sportsManService.findSpecificUser(idContact), false);
-        return "redirect:/user";
-    }
+	@RequestMapping(value = "/removeContact{id}", method = RequestMethod.POST)
+	public String removeContact(@RequestParam(value = "contact") Long idContact,
+			Principal principal) {
+		sportsManService.addOrRemoveContacts(sportsManService.findCurrentUser(principal.getName()),
+				sportsManService.findSpecificUser(idContact), false);
+		return "redirect:/user";
+	}
 
-    @RequestMapping(value = "/applyAsConfirmedUser", method = RequestMethod.GET)
-    public String applyAsConfirmedUser(Principal principal) {
-        sportsManService.applyForConfirmedRole(sportsManService.findCurrentUser(principal.getName()));
-        return "redirect:/user";
-    }
+	@RequestMapping(value = "/applyAsConfirmedUser", method = RequestMethod.GET)
+	public String applyAsConfirmedUser(Principal principal) {
+		sportsManService.applyForConfirmedRole(sportsManService.findCurrentUser(principal.getName()));
+		return "redirect:/user";
+	}
 
 }
